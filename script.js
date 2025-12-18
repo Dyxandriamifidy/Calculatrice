d2ae10be1716dc8e69d3258e54aa4162e7abdaff
DyxDylan <dyxandriam@gmail.com>
2025-12-18 13:48:42 +0300

    Fonctionnalite des boutons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.addEventListener("click", () => {});
+let affiche = document.getElementById("reponseOperation");
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+
+numberBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (affiche.textContent === "0") {
+      affiche.textContent = button.textContent;
+    } else {
+      affiche.textContent += button.textContent;
+    }
+  });
+});
