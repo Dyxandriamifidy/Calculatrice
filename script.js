e31c1e4f1993078770e523b117d0670ab9bce29f
DyxDylan <dyxandriam@gmail.com>
2026-01-09 13:42:54 +0300

    Javascript changement

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let affiche = document.getElementById("reponseOperation");
 const numberBtn = document.querySelectorAll(".number");
 const operatorBtn = document.querySelectorAll(".operator");
+let premierNombre = null; // Premier nombre
+let operateur = null; // Operateur (+, -, *, /)
+let etatAttente = false; //
 
 numberBtn.forEach((button) => {
   button.addEventListener("click", () => {
-    if (affiche.textContent === "0") {
+    if (etatAttente) {
       affiche.textContent = button.textContent;
+      etatAttente = false;
     } else {
-      affiche.textContent += button.textContent;
+      affiche.textContent =
+        affiche.textContent === "0"
+          ? button.textContent
+          : affiche.textContent + button.textContent;
+    }
+  });
+});
+
+operatorBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const operation = button.textContent;
+
+    if (premierNombre === null) {
+      premierNombre = parseFloat(affiche.textContent);
+      operateur = operation;
+      etatAttente = true;
     }
   });
 });
